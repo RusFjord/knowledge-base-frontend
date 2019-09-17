4de261a3fd80c23a76ec6daafd041bee14d98c85
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-09-17 17:28:31 +0300

    Начата работа с базовой страницей

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.scss';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Новый проект: фронтенд для дипломной работы</h1>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-light u8-navbar">
+        <div className='row'>
+          <div className='col-12 col-md-3 col-lg-2'>
+        <a class="navbar-brand" href="localhost">
+          <img src={logo} height="60" class="d-inline-block align-top" alt="logo" />
+        </a>
+        </div>
+        <div className='col-12 col-md-9 col-lg-10'>
+          <h1 className='u8-navbar-title'>Система управления знаниями</h1>
+        </div>
+        </div>
+      </nav>
     </div>
   );
 }
