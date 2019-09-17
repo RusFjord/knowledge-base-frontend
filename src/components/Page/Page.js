5ebeeedd2ca07fcb4d04254559514533b4d1d9f7
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-09-17 17:40:38 +0300

    Основная страница

---
+++ src/components/Page/Page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Page.scss';
+
+export default class Page extends Component {
+    render() {
+        return (
+            <div className='u8-page'>
+                <span>ljsdcojhfjudhnc</span>
+            </div>
+        )
+    }
+}
