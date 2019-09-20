5075b23833d518020a8b6aeea1f63a05cf76ab02
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-09-20 05:56:18 +0300

    Добавлен вывод  SideMenu. Исправлены стили.

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Page.scss';
+import SideMenu from '../SideMenu/SideMenu';
+
 
 export default class Page extends Component {
     render() {
         return (
-            <div className='u8-page'>
-                <span>ljsdcojhfjudhnc</span>
+            <div className='u8-wrap'>
+                <SideMenu />
             </div>
         )
     }
