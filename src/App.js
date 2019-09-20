c42b189e6419be102f2194b6f25d44f37b5e262b
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-09-20 05:53:37 +0300

    Исправлен class на className

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Page from './components/Page/Page';
 
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light u8-navbar">
+    <div className="App h-100">
+      <nav className="navbar navbar-expand-lg navbar-light u8-navbar">
         <div className='row'>
           <div className='col-12 col-md-3 col-lg-2'>
-        <a class="navbar-brand" href="localhost">
-          <img src={logo} height="60" class="d-inline-block align-top" alt="logo" />
+        <a className="navbar-brand" href="localhost">
+          <img src={logo} height="60" className="d-inline-block align-top" alt="logo" />
         </a>
         </div>
         <div className='col-12 col-md-9 col-lg-10'>
@@ -19,9 +19,9 @@ function App() {
         </div>
       </nav>
       <Page />
-      <footer className='u8-footer'>
+      {/* <footer className='u8-footer'>
         Created by Endovitskiy Fedor (C) 2019
-      </footer>
+      </footer> */}
     </div>
   );
 }
