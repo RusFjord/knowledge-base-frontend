e3e9a9ee7d21518683d85fd5888651ec8f7dbf14
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-09-23 05:22:04 +0300

    Подключено использование роутов

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom';
 import './Page.scss';
 import SideMenu from '../SideMenu/SideMenu';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 
 
 export default class Page extends Component {
@@ -8,6 +11,12 @@ export default class Page extends Component {
         return (
             <div className='u8-wrap'>
                 <SideMenu />
+                <div >
+                    <Switch>
+                        <Route exact path = '/' component={Dashboard} />
+                        <Route path = '/login' component={Login} />
+                    </Switch>
+                </div>
             </div>
         )
     }
